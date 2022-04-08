1a68e5591cdb7399cab0af42636207ee63ef7bf9
Steven Wilson <tehhiper1@gmail.com>
2022-04-08 10:52:57 -0500

    Moving DVMProject/dvmhost into submod

--- opt/dvm/stop-dvm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PID=`pgrep dvmhost`
-pgrep dvmhost >/dev/null
-if [ $? -eq 0 ]; then
-    kill -9 $PID
-fi
-
--- opt/dvm/stop-watchdog.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PID=`pgrep dvm-watchdog.sh`
-pgrep dvm-watchdog.sh >/dev/null
-if [ $? -eq 0 ]; then
-    kill -9 $PID
-fi
-
